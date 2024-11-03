4dff7d94203fa86caac2baa82001ac62f8a2d641
Dima <dima1999ostapenko@gmail.com>
2024-11-03 12:38:20 +0200

    add

---
+++ src/basic/5.ts
@@ -0,0 +1,15 @@
+enum DayOfWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function isWeekend(day: DayOfWeek): boolean {
+  return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+}
+
+export { DayOfWeek, isWeekend };
